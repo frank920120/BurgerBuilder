c71260f4baa96e30fb77a52c30be25e10d6fdca6
Frank Fang <frankfang2014@hotmail.com>
2019-07-27 16:18:33 -0700

    initial burger builder project

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
